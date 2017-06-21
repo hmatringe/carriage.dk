83c887353f4a0480bafae59fb47ca9e1a661a80f
Matringe <hadrien.matringe@gmail.com>
2017-06-21 18:25:14 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :bikes, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:create, :update, :show] do
+      resources :payments, only: [:new, :create]
+    end
+  end
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
