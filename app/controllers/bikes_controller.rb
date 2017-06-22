65ee34fb52996bd9e80c70c946d1ff9374645879
Matringe <hadrien.matringe@gmail.com>
2017-06-22 19:43:31 +0200

    bookings create

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -6,6 +6,7 @@ class BikesController < ApplicationController
   end
 
   def show
+    @booking = Booking.new
   end
 
   def new
@@ -29,7 +30,5 @@ class BikesController < ApplicationController
   def bike_params
     params.require(:bike).permit(:color, :description_title, :description, :caption, :address, :brand_model, :brand, :brand_sku, :renter_must_have_experience_driving_cargo_bikes, :renter_must_have_experience_cycling_in_the_city, :renter_can_be_a_company, :has_seatbelt, :has_rain_cover, :box_has_bench, :is_electric, :children_seating_capacity, :gears_set, :light_set, :lock_set, :box_dimensions, :state, photo: [])
   end
-
-
 end
 
